4e749021476554cc8954a7687bfb7a042325f45f
David Pine <david.pine@microsoft.com>
2023-09-18 21:18:17 -0500

    tweaks

--- src/ProfanityFilter.Services/MatchEvaluators.cs
+++ src/ProfanityFilter.Services/MatchEvaluators.cs
@@ -11,72 +11,72 @@ internal static class MatchEvaluators
     /// <summary>
     /// A <see cref="MatchEvaluator"/> that replaces a matched string with asterisks.
     /// </summary>
-    internal static MatchEvaluator AsteriskEvaluator = new(
-        static match =>
+    internal static MatchEvaluator AsteriskEvaluator =
+        static (Match match) =>
         {
             var result = new string('*', match.Length);
 
             return result;
-        });
+        };
 
     
     /// <summary>
     /// A <see cref="MatchEvaluator"/> that replaces a matched profanity with a random emoji from 
     /// a predefined list of hand-selected replacements.
     /// </summary>
-    internal static MatchEvaluator EmojiEvaluator = new(
-        static match =>
+    internal static MatchEvaluator EmojiEvaluator =
+        static (Match match) =>
         {
             var emoji = Emoji.HandSelectedReplacements;
 
             return emoji[Random.Shared.Next(emoji.Length)];
-        });
+        };
 
     /// <summary>
     /// A <see cref="MatchEvaluator"/> that replaces a matched string with a random number of asterisks.
     /// The number of asterisks is between 1 and the length of the matched string.
     /// </summary>
-    internal static MatchEvaluator RandomAsteriskEvaluator = new(
-        static match =>
+    internal static MatchEvaluator RandomAsteriskEvaluator =
+        static (Match match) =>
         {
             var result = new string('*', Random.Shared.Next(1, match.Length));
 
             return result;
-        });
+        };
     
     
     /// <summary>
     /// A <see cref="MatchEvaluator"/> that replaces the characters between the first and last 
     /// characters of a match with asterisks.
     /// </summary>
-    internal static MatchEvaluator MiddleAsteriskEvaluator = new(
-        static match =>
+    internal static MatchEvaluator MiddleAsteriskEvaluator =
+        static (Match match) =>
         {
             var value = match.ValueSpan;
 
             var result = $"{value[0]}{new string('*', match.Length - 2)}{value[^1]}";
 
             return result;
-        });
+        };
 
     /// <summary>
     /// A <see cref="MatchEvaluator"/> that replaces the middle of a swear word with the 🤬 emoji.
     /// </summary>
-    internal static MatchEvaluator MiddleSwearEmojiEvaluator = new(
-        static match =>
+    internal static MatchEvaluator MiddleSwearEmojiEvaluator =
+        static (Match match) =>
         {
             var value = match.ValueSpan;
 
             var result = $"{value[0]}🤬{value[^1]}";
 
             return result;
-        });
+        };
 
     /// <summary>
     /// A <see cref="MatchEvaluator"/> that replaces vowels in a string with asterisks (*).
     /// </summary>
-    internal static MatchEvaluator VowelAsteriskEvaluator = new(
-        static match =>
+    internal static MatchEvaluator VowelAsteriskEvaluator =
+        static (Match match) =>
         {
             var value = match.ValueSpan;
 
@@ -89,5 +89,5 @@ internal static class MatchEvaluators
             }
 
             return result.ToString();
-        });
+        };
 }
