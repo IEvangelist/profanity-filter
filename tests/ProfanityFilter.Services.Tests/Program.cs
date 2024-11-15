490b42be343cc193ef425ab624622410227d3159
Amaury Levé <amauryleve@microsoft.com>
2024-11-15 10:14:41 +0100

    Add dotnet test support keeping Program.cs

---
+++ tests/ProfanityFilter.Services.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) David Pine. All rights reserved.
+// Licensed under the MIT License.
+
+var builder = await TestApplication.CreateBuilderAsync(args);
+
+// Instead of manually referencing the extensions, we are using the MTP MSBuild feature that
+// auto-detects extensions exposing hooks and hide them all under this extension method.
+builder.AddSelfRegisteredExtensions(args);
+
+var app = await builder.BuildAsync();
+
+return await app.RunAsync();
